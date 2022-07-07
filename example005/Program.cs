39f399c9e2af3833e2f6539ce1dee5d1d4e05e1c
Vbasov <0120912@mail.ru>
2022-07-07 10:05:30 +0300

    prog

---
+++ example005/Program.cs
@@ -0,0 +1,7 @@
+﻿string username = Console.ReadLine();
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Ура, это же Маша!");
+} else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
